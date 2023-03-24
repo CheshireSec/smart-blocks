e7a5bc25bbd54f689278926a9af29f1bc0619bb2
brokenpanda <brokenpanda@protonmail.com>
2023-03-24 10:15:22 +0100

    removed initial posts, added ethereum analysis

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Smart Blocks Coin Analytics';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Smart Blocks Coin Analytics - The #1 Cryptocurrency Analysis Blog';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
